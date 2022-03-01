85eeebfc34b47b5afcf9c938def2f1b876547412
Zueon <maineon0701@gmail.com>
2022-03-01 15:35:23 +0900

    vue3 template with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-strongly-recommended', 'airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'never',
+      },
+    ],
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>
+    {{ msg }}
+  </h1>
+  <input
+    type="text"
+    v-model="msg" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello',
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
